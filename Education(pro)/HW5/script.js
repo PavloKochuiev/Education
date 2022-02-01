43a79410ba40cd8737780cdc77ec79f0d233e8c7
Pavlo Kochuiev <pashadagilis@gmail.com>
2022-02-01 02:16:49 +0200

    HW5 is Done

--- Education(pro)/HW5/script.js
+++ Education(pro)/HW5/script.js
@@ -1,21 +1,36 @@
+// 1.Написать функцию, которая вычисляет площадь круга. 
+// Нашел что-то, но не понимаю как это работает.
+
 function circle(radius) {
     this.radius = radius;
     this.area = function () {
         return Math.PI * this.radius * this.radius;
     };
-    this.perimeter = function () {
-        return 2 * Math.PI * this.radius;
-    };
 }
-let c = new circle(4);
-console.log('Area', c.area().toFixed(2));
-console.log('Perimeter', c.perimeter().toFixed(2));
+let c = new circle(3);
+console.log('Area =', c.area().toFixed(2));
 
 
-function average(first, second) {
-    return (first + second) / 2;
+
+// 2. Написать функцию, которая вычисляет длину окружности.
+
+function calculateCircumference(radius) {
+    return 2 * Math.PI * radius;
 }
-console.log("Average", average(6, 9));
+console.log("Circumference:", calculateCircumference(1));
+
+
+
+// 3. Написать функцию, которая вычисляет среднее арифметическое двух чисел.
+
+function average(a, b) {
+    return (a + b) / 2;
+}
+console.log("Average: ", average(20, 30));
+
+
+
+//  4. Написать функцию calc(x, y, action), которая получает 3 аргумента: числа x и y, а также строку action. В переменной action может быть: +, -, *, /, %, ^. Вывести результат математического действия, указанного в переменной action.
 
 
 function calc(a, b, action) {
@@ -37,11 +52,10 @@ function calc(a, b, action) {
             break;
     }
 };
-
-console.log(calc(8, 3, "+"));
-console.log(calc(8, 3, "-"));
-console.log(calc(8, 3, "*"));
-console.log(calc(8, 3, "/"));
-console.log(calc(8, 3, "%"));
-console.log(calc(8, 3, "^"));
-console.log(calc(8, 3, "0"));
+console.log(calc(10, 5, "+"));
+console.log(calc(10, 5, "-"));
+console.log(calc(10, 5, "*"));
+console.log(calc(10, 5, "/"));
+console.log(calc(10, 5, "%"));
+console.log(calc(10, 5, "^"));
+console.log(calc(10, 5, "0"));
