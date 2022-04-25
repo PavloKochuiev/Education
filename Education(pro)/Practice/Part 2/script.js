7bffa8e817b6822f80622aeb03e6a87deb17f00e
Pavlo Kochuiev <pashadagilis@gmail.com>
2022-04-25 16:09:11 +0300

    Add all.

---
+++ Education(pro)/Practice/Part 2/script.js
@@ -0,0 +1,27 @@
+console.time("Request");
+
+console.log("Hello, world!");
+
+console.info("Console, INFO!");
+
+console.error("Error, 404!");
+
+console.count("console, count!");
+console.count("console, count!");
+console.count("console, count!");
+
+console.timeEnd("Request");
+
+let color = "green";
+console.log(color);
+
+color = "blue";
+console.log(color);
+
+const first_name = prompt("What is your name?");
+console.log(first_name);
+
+
+
+const a = 1;
+console.log("Type of a: " + typeof a);
