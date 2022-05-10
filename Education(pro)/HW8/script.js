b93e6b5233177088f307211a733f00ead4dcb7dc
Pavlo Kochuiev <pashadagilis@gmail.com>
2022-05-10 13:22:39 +0300

    HW 8 is done

--- Education(pro)/HW8/script.js
+++ Education(pro)/HW8/script.js
@@ -1,11 +1,8 @@
+"use strict";
+
 // Написать функцию “глубокого” копирования.Функция принимает один параметр и возвращает его копию “по значению”. 
-// В функцию можно передать параметр любого типа. 
-// Если передали объект, то предусмотреть ситуацию, когда свойствами этого объекта будут объекты или массивы.
-// Если передали массив, то предусмотреть ситуацию, когда элементами этого массива будут объекты или массивы.
-// Если передали примитив - вернуть его.
 
 // a !== b 
-// a.c !== b.c
 
 const userName = "Pasha";
 
@@ -20,49 +17,16 @@ const worker = {
     }
 };
 
-// function deepCopy(origin) {
-
-//     if (origin instanceof Object) {
-//         return origin;
-//     }
-
-//     let target = {};
-
-//     for (let key in origin) {
-//         let temp = origin[key];
-
-//         if (typeof temp === "object" && temp !== null) {
-//             target[key] = deepCopy(temp);
-//         } else {
-//             target[key] = temp;
-//         }
-//     }
-//     return target;
-// }
-
-// let workerCopy = deepCopy(worker);
-// console.log("Worker: ", worker);
-// console.log("Worker Copy: ", workerCopy);
-
-
-
-// let userNameCopy = deepCopy(userName);
-// console.log("userName: ", userName);
-// console.log("userNameCopy: ", userNameCopy);
-
-
-
 function deepCopy(obj) {
-    const copyObj = Array.isArray(obj) ? [] : {};
-    for (const key in obj) {
-        if (obj.hasOwnProperty(key)) {
-            if ("object" === typeof obj[key] && obj[key] !== null)
-                copyObj[key] = deepCopy(obj[key]);
-            else
-                copyObj[key] = obj[key];
+    const clObj = {};
+    for (const i in obj) {
+        if (obj[i] instanceof Object) {
+            clObj[i] = deepCopy(obj[i]);
+            continue;
         }
+        clObj[i] = obj[i];
     }
-    return copyObj;
+    return clObj;
 }
 
 
@@ -73,9 +37,8 @@ console.log("Worker Copy: ", workerCopy);
 console.log("Worker === WorkerCopy", worker === workerCopy);
 
 const userNameCopy = deepCopy(userName);
-console.log("userName: ", userName);
-console.log("userNameCopy: ", userNameCopy);
 console.log("userName === userNameCopy", userName === userNameCopy);
+
 
 
 
