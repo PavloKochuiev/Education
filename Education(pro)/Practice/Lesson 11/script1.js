cd870e6f5e3af0f7661ec1efddd4ac8b2de80217
Pavlo Kochuiev <pashadagilis@gmail.com>
2022-05-14 23:35:14 +0300

    add lesson 11

--- Education(pro)/Practice/Lesson 11/script1.js
+++ Education(pro)/Practice/Lesson 11/script1.js
@@ -1,2 +1,28 @@
 "use strict"
 
+let id = 1;
+
+function createUser(email) {
+    return {
+        id: id++,
+        email
+    }
+}
+
+const users = [
+    createUser("Eugene"),
+    createUser("Roman"),
+    createUser("Petro"),
+]
+
+console.table(users);
+
+function createUserParagraph(user) {
+
+}
+
+for (const user of users) {
+    const p = createUserParagraph(user);
+    
+}
+
