4aaa9beeaab9d543d2de0819d0e8220c8c6d1157
Pavlo Kochuiev <pashadagilis@gmail.com>
2022-05-16 22:14:47 +0300

    HW10 version 2

--- Education(pro)/HW10/script.js
+++ Education(pro)/HW10/script.js
@@ -5,32 +5,53 @@
 
 let array = [1, 2, [1.1, 1.2, 1.3], 3];
 
-generateList(array);
+// generateList(array);
 
-function generateList(array) {
-    let element = "<ul>";
-    const count = array.length;
+// function generateList(array) {
+//     let element = "<ul>";
 
-    for (let i = 0; i < count; ++i) {
-        let row = array[i];
+//     for (let i = 0; i < array.length; ++i) {
+//         let row = array[i];
 
-        if (Array.isArray(row)) {
-            element = element + "<li><ul>";
+//         if (Array.isArray(row)) {
+//             element = element + "<li><ul>";
 
-            for (let i = 0; i < row.length; ++i) {
-                element = element + `<li>${row[i]}</li>`;
-            }
-            element = element + "</ul></li>";
+//             for (let i = 0; i < row.length; ++i) {
+//                 element = element + `<li>${row[i]}</li>`;
+//             }
+//             element = element + "</ul></li>";
+//         }
+
+//         else
+//             element = element + `<li>${row}</li>`;
+//     }
+
+//     element = element + "</ul>";
+
+//     document.body.innerHTML = element;
+// }
+
+// новое решение >>> 
+
+let generateList = function (items) {
+    return items.reduce((ul, item) => {
+
+        let li = document.createElement('li');
+
+        if (!Array.isArray(item)) {
+            li.innerText = item;
+        } else {
+            let newUl = generateList(item);
+            li.appendChild(newUl);
         }
 
-        else
-            element = element + `<li>${row}</li>`;
-    }
+        ul.appendChild(li);
+        return ul;
 
-    element = element + "</ul>";
-
-    document.body.innerHTML = element;
+    }, document.createElement('ul'));
 }
+
+document.body.appendChild(generateList(array));
 
 
 
