37d5991b154c58a76acd02cb6fcf4f5d8c327ff5
Pavlo Kochuiev <pashadagilis@gmail.com>
2022-05-23 22:40:14 +0300

    task 1 is done

--- Education(pro)/HW12/script.js
+++ Education(pro)/HW12/script.js
@@ -1,1 +1,18 @@
 'use strict'
+
+// Дано 5 квадратов, внутри каждого из которых изначально вписана цифра 0.
+// При клике на любой квадрат цифра в нем увеличивается на один.
+
+let squares = document.querySelectorAll("div");
+
+        for (let i = 0; i < squares.length; ++i) {
+            squares[i].addEventListener("click", counter);
+        }
+
+        function counter() {
+            this.innerText = ++this.innerText;
+            console.log(this.innerText);
+        }
+
+// Дано 5 квадратов. Каждый по клику меняет цвет (синий -> зеленый -> желтый)
+
