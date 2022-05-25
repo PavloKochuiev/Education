58b43bc255a07015753cf2c56a44247e2d349b13
Pavlo Kochuiev <pashadagilis@gmail.com>
2022-05-25 03:17:27 +0300

    HW13 is done

--- Education(pro)/HW13/script.js
+++ Education(pro)/HW13/script.js
@@ -14,21 +14,79 @@
 
 // Если пользователь ввел невалидные данные, поле для ввода должно выделиться красным цветом.
 
+const form = document.querySelector("form");
+const button = document.querySelector("form button");
 const emailInput = document.querySelector("form input[type=text]");
 const passwordInput = document.querySelector("form input[type=password]");
 
-const formButton = document.querySelector("form");
+button.disabled = true;
 
-formButton.addEventListener("submit", event => {
+function isValidEmail(email) {
+    return /^([a-z0-9_-]+\.)*[a-z0-9_-]+@[a-z0-9_-]+(\.[a-z0-9_-]+)*\.[a-z]{2,6}$/.test(email);
+};
+
+function isValidPassword(password) {
+    return /(?=.*[0-9])(?=.*[!@#$%^&*])(?=.*[a-z])(?=.*[A-Z])[0-9a-zA-Z!@#$%^&*]{8,}/.test(password);
+};
+
+let validEmail = false;
+let validPassword = false;
+
+emailInput.addEventListener("input", event => {
+    if (validPassword) {
+        validEmail = isValidEmail(event.target.value);
+    }
+
+    button.disabled = !validEmail || !validPassword;
+})
+
+passwordInput.addEventListener("input", event => {
+    if (validEmail) {
+        validPassword = isValidPassword(event.target.value);
+    }
+
+    button.disabled = !validEmail || !validPassword;
+})
+
+emailInput.addEventListener("blur", event => {
+    const { value } = event.target;
+    validEmail = isValidEmail(value)
+
+    if (value !== '' && !validEmail) {
+        event.target.className = "invalid";
+    }
+
+    button.disabled = !validEmail || !validPassword;
+});
+
+emailInput.addEventListener("focus", event => {
+    event.target.className = '';
+});
+
+passwordInput.addEventListener("blur", event => {
+    const { value } = event.target;
+    validPassword = isValidPassword(value);
+
+    if (value !== '' && !validPassword) {
+        event.target.className = "invalid";
+    }
+
+    button.disabled = !validEmail || !validPassword;
+});
+
+passwordInput.addEventListener("focus", event => {
+    event.target.className = ''; 
+});
+
+form.addEventListener("submit", event => {
     event.preventDefault();
 
     const email = emailInput.value;
-
     const password = passwordInput.value;
+    console.log({ email, password });
 
-    const data = { email, password };
-    console.log(JSON.stringify(data));
+    emailInput.value = '';
+    passwordInput.value = '';
 
-    emailInput.value = "";
-    passwordInput.value = "";
+    button.disabled = true;
 })
