afc8d9d4402d4251470a279c65094ab622eab60a
Pavlo Kochuiev <pashadagilis@gmail.com>
2022-06-02 01:02:51 +0300

    lesson 18 is done

--- Education(pro)/Practice/Lesson 18/script1.js
+++ Education(pro)/Practice/Lesson 18/script1.js
@@ -1,39 +1,75 @@
 "use strict";
 
-// Data 
+// Data
 // HTTP // HTTPS
 
 // GET => получение данных (например: дать список последних пяти новостей)
 // POST => отправка данных (например: отправка сообщения в чате)
-// PUT => редактирование сущности на стороне backend 
+// PUT => редактирование сущности на стороне backend
 // DELETE => удаление сущности на стороне backend
 
-const API_URL = "https://rickandmortyapi.com/api/character";
+const API_URL = "https://rickandmortyapi.com/api/character?page=1";
 
 // XHR - XML HTTP REQUEST // старый способ
 // fetch - ES6 - Promise // новый способ, который все используют
 
-const xhr = new XMLHttpRequest(); // создаем новый объект-запрос
-xhr.responseType = "json"; // можем указать response type для того чтобы сразу получить json
+const loadButton = document.querySelector(".wrapper button");
+const content = document.querySelector(".wrapper .content");
 
-xhr.onload = function() { // должны подписаться на событие onload и проверить статус
-    console.log("onload", {
-        status: xhr.status, // 200 = OK 
-        text: xhr.statusText, 
-        response: xhr.response,
-        responseType: typeof xhr.response  
-    });
+function renderList(items) {
+  const ul = document.createElement("ol");
 
-    if (xhr.status !== 200) {
-        console.error(`Error ${xhr.status}: ${xhr.statusText}`);
-    } else {
-        console.log(xhr.response);
-    }
+  for (const item of items) {
+    const listElement = document.createElement("li");
+    listElement.textContent = item.name;
+
+    ul.appendChild(listElement);
+  }
+
+  content.innerHTML = "";
+  content.appendChild(ul);
 }
 
-xhr.onerror = function(event) { // обрабатываем ошибки
-    console.log("Error", event); // 404 = NOT FOUND
-};
+function setButtonLoadingState() {
+  loadButton.textContent = "Loading...";
+  loadButton.disabled = true;
+}
 
-xhr.open("GET", API_URL); // необходимо открыть запрос
-xhr.send(); // необходимо вызвать send
+function setButtonInitialState() {
+  loadButton.textContent = "Load";
+  loadButton.disabled = false;
+}
+
+loadButton.addEventListener("click", () => {
+  load({
+    url: API_URL,
+    onLoadStart: setButtonInitialState,
+    onLoadSuccess: (data) => {
+      renderList(data.results);
+      setButtonInitialState();
+    },
+    onLoadError: () => {
+      alert("Error loading data!");
+      setButtonInitialState();
+    },
+  })
+});
+
+function load(props) {
+  const xhr = new XMLHttpRequest(); // создаем новый объект-запрос
+  xhr.responseType = "json"; // можем указать response type для того чтобы сразу получить json
+
+  xhr.onload = function () {
+    // должны подписаться на событие onload и проверить статус
+    if (xhr.status === 200) {
+      props.onLoadSuccess(xhr.response);
+    } else {
+      props.onLoadError();
+    }
+  };
+
+  xhr.onerror = props.onLoadError;
+
+  xhr.open("GET", props.url); // необходимо открыть запрос
+  xhr.send(); // необходимо вызвать send
+}
