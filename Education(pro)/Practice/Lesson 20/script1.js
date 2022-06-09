6a666805801823f2841e524985a68b3a51c7c04f
Pavlo Kochuiev <pashadagilis@gmail.com>
2022-06-09 23:13:41 +0300

    ading lesson 20

--- Education(pro)/Practice/Lesson 20/script1.js
+++ Education(pro)/Practice/Lesson 20/script1.js
@@ -1,14 +1,17 @@
 "use strict";
 
 function loadPost(postNumber) {
-  return fetch(`https://jsonplaceholder.typicode.com/posts/${postNumber}`)
-  .then((response) => {
-    if (response.status === 200) {
-      return response.json();
+  return fetch(`https://jsonplaceholder.typicode.com/posts/${postNumber}`).then(
+    (response) => {
+      if (response.status === 200) {
+        return response.json();
+      }
+      const error = new Error(`Error loading post with ID ${postNumber}`);
+      return new Promise((resolve, reject) => reject(error));
     }
-    return new Promise((resolve, reject) => reject());
-    })
+  );
 }
+
 
 // Последовательная загрузка
 loadPost(1)
@@ -23,12 +26,22 @@ loadPost(1)
   .then((data) => console.log("Post 5", data));
 
 // Параллельная загрузка
-  Promise.all([
-    loadPost(1),
-    loadPost(2),
-    loadPost(3),
-    loadPost(4),
-    loadPost(5),
-    loadPost(500)
-  ])
-    .then(data => console.log("Promise.all:", data));
+Promise.all([
+  loadPost(1),
+  loadPost(2),
+  loadPost(3),
+  loadPost(4),
+  loadPost(5),
+  loadPost(500),
+])
+  .then((data) => console.log("Promise.all Then", data))
+  .catch((error) => console.log("Promise.all Catch", error));
+
+///
+
+const posts = [1, 2, 3, 4, 5, 398];
+
+Promise.allSettled(posts.map((postId) => loadPost(postId)))
+  .then((result) => console.log("result", result))
+  .catch((error) => console.log("Promise.all Catch", error));
+
--- Education(pro)/Practice/Lesson 20/script2.js
+++ Education(pro)/Practice/Lesson 20/script2.js
@@ -1,2 +1,23 @@
 "use strict";
 
+// промисификация
+
+const getLocationButton = document.querySelector("#geoLocation");
+getLocationButton.addEventListener('click', (event) => {
+    event.target.disabled = true;
+
+    getCurrentPosition()
+        .then(position => console.log(position))
+        .catch(error => console.log(error))
+        .finally(() => event.target.disabled = false);
+
+    // navigator.geolocation.getCurrentPosition(
+    //    (data) => console.log("OnSuccess", data),
+    //    (error) => console.log("OnError", error)
+})
+
+function getCurrentPosition() {
+    return new Promise((resolve, reject) => {
+            navigator.geolocation.getCurrentPosition(resolve, reject)
+    })
+}
