c8f76b4bff8b39fec7f305f5a61ff3f5f9386118
Pavlo Kochuiev <pashadagilis@gmail.com>
2022-06-17 23:44:50 +0300

    Added Lesson 22

--- Education(pro)/Practice/Lesson 22/script1.js
+++ Education(pro)/Practice/Lesson 22/script1.js
@@ -1,4 +1,18 @@
 "use strict";
 
+const maxPages = 12; 
 
+async function getMaxPages() {
+    const response = fetch("https://rickandmortyapi.com/api/character/page=1);
+    if (!response.ok) return null;
+
+    const data = await response.json();
+    return data.info.pages;
+}
+
+async function run() {
+
+}
+
+run();
 
