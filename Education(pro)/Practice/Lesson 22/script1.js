5e15ab32dc0c404f9310cd7ffaf28420e35f40e1
Pavlo Kochuiev <pashadagilis@gmail.com>
2022-06-18 02:55:00 +0300

    Added lesson 22 part 1

--- Education(pro)/Practice/Lesson 22/script1.js
+++ Education(pro)/Practice/Lesson 22/script1.js
@@ -1,18 +1,47 @@
 "use strict";
 
-const maxPages = 12; 
+// ООП 
+// Инкапсуляция / Наследование / Полиморфизм 
 
-async function getMaxPages() {
-    const response = fetch("https://rickandmortyapi.com/api/character/page=1);
-    if (!response.ok) return null;
-
-    const data = await response.json();
-    return data.info.pages;
+const user = {
+    name: "Dima",
+    walk() {
+        console.log("I am walking", this);
+    }
 }
 
-async function run() {
+// Прототип = ссылка на родителя
+const developer = {
+    skills: ["JavaScript", "HTML", "CSS"],
+    // Прототип - object
+    writeCode() {
+        console.log("Coding...", this);
+    }
+};
 
+developer.__proto__ = user;
+
+const manager = {
+    teams: ["FrontEnd", "Design"],
+    talkToCustomer() {
+        console.log("Meeting...");
+    }
 }
 
-run();
+manager.__proto__ = developer;
+
+// console.log("user name", user.name);
+// console.log("developer name", developer.name); // developer => proto => user => name => proro Object => proto null
+
+// user.walk();
+// developer.walk();
+// developer.writeCode();
+
+
+
+// List -> characters
+// Data to display
+// Load more
+
+// List -> episodes
 
